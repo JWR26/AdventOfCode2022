0d986ece3bc851143482bec86ad675b17f09a951
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2022-12-04 09:03:09 +0000

    day4
    solutions up to day 4

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.VisualBasic;
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 
 class Solution
@@ -13,7 +15,7 @@ class Solution
         Console.WriteLine(new Day1().answer(3));
         sw.Stop();
 
-        Console.WriteLine("Day 1 execution time: {0}ms.", sw.ElapsedMilliseconds);
+        Console.WriteLine("Day 1 execution time: {0}ms. Ticks {1}", sw.ElapsedMilliseconds, sw.ElapsedTicks);
 
         Day2 d2 = new Day2();
         sw.Restart();
@@ -21,7 +23,7 @@ class Solution
         Console.WriteLine(d2.answer2());
         sw.Stop();
 
-        Console.WriteLine("Day 2 execution time: {0}ms.", sw.ElapsedMilliseconds);
+        Console.WriteLine("Day 2 execution time: {0}ms. Ticks {1}", sw.ElapsedMilliseconds, sw.ElapsedTicks);
 
         Day3 d3 = new Day3();
         sw.Restart();
@@ -29,7 +31,14 @@ class Solution
         Console.WriteLine(d3.answer2());
         sw.Stop();
 
-        Console.WriteLine("Day 3 execution time: {0}ms.", sw.ElapsedMilliseconds);
+        Console.WriteLine("Day 3 execution time: {0}ms. Ticks {1}", sw.ElapsedMilliseconds, sw.ElapsedTicks);
+
+        Day4 d4 = new Day4();
+        sw.Restart();
+        Console.WriteLine(d4.answer());
+        sw.Stop();
+
+        Console.WriteLine("Day 4 execution time: {0}ms. Ticks {1}", sw.ElapsedMilliseconds, sw.ElapsedTicks);
 
         Console.ReadLine();
     } 
@@ -39,7 +48,7 @@ class Day1
 {
     public int answer(int numberOfElves)
     {
-        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_01_01.txt").Split("\n\n");
+        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_01.txt").Split("\n\n");
 
         List<int> calories = new List<int>() { 0 };
 
@@ -73,7 +82,7 @@ class Day2
 {
     public int answer()
     {
-        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_02_01.txt").Split("\n");
+        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_02.txt").Split("\n");
 
         int score = 0;
 
@@ -90,7 +99,7 @@ class Day2
 
     public int answer2()
     {
-        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_02_01.txt").Split("\n");
+        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_02.txt").Split("\n");
 
         int score = 0;
 
@@ -160,11 +169,116 @@ class Day3
 {
     public int answer()
     {
-        return 0;
+        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_03.txt").Split("\n");
+
+        int sum = 0;
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            char commonItem;
+            
+            string secondHalf = data[i].Substring(data[i].Length / 2);
+
+            for(int j = 0; j < secondHalf.Length; j++)
+            {
+                if (secondHalf.Contains(data[i][j]))
+                {
+                    commonItem = data[i][j];
+                    sum += priority(commonItem);
+                    break;
+                }                
+            }
+        }
+
+        return sum;
     }
 
     public int answer2()
     {
-        return 0;
+        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_03.txt").Split("\n");
+
+        int sum = 0;
+
+        for (int i = 0; i < data.Length; i+=3)
+        {
+            char commonItem;
+
+            string elf1 = data[i];
+            string elf2 = data[i + 1];
+            string elf3 = data[i + 2];
+
+            for (int j = 0; j < elf1.Length; j++)
+            {
+                if (elf2.Contains(elf1[j]) && elf3.Contains(elf1[j]))
+                {
+                    commonItem = elf1[j];
+                    sum += priority(commonItem);
+                    break;
+                }
+            }
+        }
+
+        return sum;
+    }
+
+    private int priority(char c) => Char.IsLower(c) ? ( (int)c - (int)'a' + 1 ) : ( (int)c - (int)'A' + 27 );
+}
+
+class Day4
+{
+    public (int, int) answer()
+    {
+        string[] data = File.ReadAllText("C:\\Users\\jonat\\source\\repos\\AdventOfCode2022\\input_04.txt").Split("\n");
+
+        (int containing, int overlapping) sum = (0, 0);
+
+        for(int i=0; i<data.Length; i++)
+        {
+            string[] strings = data[i].Replace(',', '-').Split("-");
+
+            int[] ints = toIntArray(strings);
+
+            sum.containing += elfCovered(ints) ? 1 : 0;
+            sum.overlapping += elfOverlapped(ints) || elfCovered(ints) ? 1 : 0;
+        }
+
+        return sum;
+    }
+
+    int[] toIntArray(string[] strings)
+    {
+        int[] ints = new int[strings.Length];
+        
+        for(int i = 0; i < ints.Length; i++)
+        {
+            ints[i] = Convert.ToInt32(strings[i]);
+        }
+
+        return ints;
+    }
+    
+    bool elfCovered(int[] ints)
+    {
+        if (ints[0] <= ints[2] && ints[1] >= ints[3])
+        {
+            return true;
+        }
+
+        if (ints[0] >= ints[2] && ints[1] <= ints[3])
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    bool elfOverlapped(int[] ints)
+    {
+        if (ints[0] <= ints[2] && ints[1] >= ints[2] || ints[0] <= ints[3] && ints[1] >= ints[3])
+        {
+            return true;
+        }
+
+        return false;
     }
 }
